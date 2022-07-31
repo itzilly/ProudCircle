d05c233d6adc2beaa6153b6ca132f09ce2b730b0
itzilly <itzillyum@gmail.com>
2022-07-31 15:56:33 -0600

    Create fs.py
    Adds feature for getting all extensions for the bot. Creates a place to add more logic based on the file system in the future.

---
+++ discord/bot/util/fs.py
@@ -0,0 +1,9 @@
+import os
+
+def get_all_extensions():
+	ext = []
+	for file in os.listdir('./extensions'):
+		if file.endswith('.py'):
+			ext.append(f"extensions.{file.replace(".py", '')}")
+	logging.debug(f"Found {len(etx)} extension(s): {[f for f in ext]}")
+	return etx
