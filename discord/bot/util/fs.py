2d206fe23a811c96f22f0bc359c0b55933b34586
itzilly <itzillyum@gmail.com>
2022-07-31 16:05:35 -0600

    Fix unmatched quotations

--- discord/bot/util/fs.py
+++ discord/bot/util/fs.py
@@ -4,6 +4,6 @@ def get_all_extensions():
 	ext = []
 	for file in os.listdir('./extensions'):
 		if file.endswith('.py'):
-			ext.append(f"extensions.{file.replace(".py", '')}")
+			ext.append(f"extensions.{file.replace('.py', '')}")
 	logging.debug(f"Found {len(etx)} extension(s): {[f for f in ext]}")
 	return etx
