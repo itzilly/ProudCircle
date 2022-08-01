7ec5ec3a15b5197d4325fe5b6c47f13ff97467de
itzilly <itzillyum@gmail.com>
2022-07-31 21:01:31 -0600

    Add config path

--- discord/bot/util/fs.py
+++ discord/bot/util/fs.py
@@ -7,4 +7,7 @@ def get_all_extensions():
 		if file.endswith('.py'):
 			ext.append(f"extensions.{file.replace('.py', '')}")
 	logging.debug(f"Found {len(ext)} extension(s): {[f for f in ext]}")
-	return etx
+	return ext
+
+def get_main_config_path():
+	return './data/config.yml'
