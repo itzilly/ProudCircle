c4d79706d279517f471ebfca7610ac733d8dbbca
itzilly <itzillyum@gmail.com>
2022-08-03 06:21:18 -0600

    Fix indentation error

--- discord/bot/util/fs.py
+++ discord/bot/util/fs.py
@@ -2,12 +2,12 @@ import os
 import logging
 
 def get_all_extensions():
-	ext = []
-	for file in os.listdir('./extensions'):
-		if file.endswith('.py'):
-			ext.append(f"extensions.{file.replace('.py', '')}")
-	logging.debug(f"Found {len(ext)} extension(s): {[f for f in ext]}")
-	return ext
+    ext = []
+    for file in os.listdir('./extensions'):
+        if file.endswith('.py'):
+            ext.append(f"extensions.{file.replace('.py', '')}")
+    logging.debug(f"Found {len(ext)} extension(s): {[f for f in ext]}")
+    return ext
 
 def get_main_config_path():
-	return './data/config.yml'
+    return './data/config.yml'
