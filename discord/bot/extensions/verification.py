00ae21bc6d1ee363bfc7c30c8314f1aaf66d43fe
itzilly <itzillyum@gmail.com>
2022-08-06 03:01:51 -0600

    Add verified role on reaction

--- discord/bot/extensions/verification.py
+++ discord/bot/extensions/verification.py
@@ -10,23 +10,19 @@ class VerificationListener(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.message_id = Settings.config['discord']['message_ids']['verification_id']
-        self.emoji_to_role = {
-            discord.PartialEmoji(name='☑️'): Settings.config['discord']['role_ids']['verified'],
-            discord.PartialEmoji(name='☑'): Settings.config['discord']['role_ids']['guild_member'],
-        }
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         if payload.message_id != self.message_id:
             return
-
-        try:
-            verified_role_id = self.emoji_to_role[payload.emoji]
-        except KeyError:
-            return
-
-        verified_role = self.bot.get_guild(payload.guild_id).get_role(verified_role_id)
-        await payload.member.add_roles(verified_role)
+        if payload.emoji.name == "☑️":
+            verified_role_id = Settings.config['discord']['role_ids']['verified']
+            verified_role = self.bot.get_guild(payload.guild_id).get_role(verified_role_id)
+            try:
+                await payload.member.add_roles(verified_role)
+                logging.info(f"User {payload.member.id} ({payload.member.name}#{payload.member.discriminator}) has just been verified!")
+            except Exception as e:
+                logging.critical(e)
 
 
 async def setup(bot):
