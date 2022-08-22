a34adb0983b7abfd4b81e50d0f9635c4238c888e
itzilly <itzillyum@gmail.com>
2022-08-22 05:19:24 -0600

    Fix command running with no permissions

--- discord/bot/extensions/run_command.py
+++ discord/bot/extensions/run_command.py
@@ -15,10 +15,12 @@ class RunCommand(commands.GroupCog, name="run"):
 
     @app_commands.command(name="promotions", description="Calculates weekly GEXP and promotes members")
     async def promotion_task(self, interaction: discord.Interaction, flags: str=None):
+        await interaction.user.add_roles()
         subcommand_name = "/run promotions"
         logging.debug(f"COMMANDS | User {interaction.user.id} ran command '{subcommand_name}'")
         if not util.command_helper.check_permission(interaction.user, level="any"):
             await interaction.response.send_message(embed=util.command_helper.no_perm_embed())
+            return
         promotions_task = PromoteTask()
         await promotions_task.run(interaction)
         if promotions_task.errored:
