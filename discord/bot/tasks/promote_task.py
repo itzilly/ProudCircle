76d8425ead4a3b5ca7b46bb2f602b1905707da32
itzilly <itzillyum@gmail.com>
2022-08-29 05:31:34 -0600

    Automatically add roles

--- discord/bot/tasks/promote_task.py
+++ discord/bot/tasks/promote_task.py
@@ -5,16 +5,15 @@ import discord
 import requests
 
 from util.mcign import MCIGN
-from datetime import datetime
+from datetime import datetime, timedelta
 from util.config_handler import Settings
-from util.linked_database import LinkedDatabase
 from util.error_embeds import ApiErrorEmbed, ConfigErrorEmbed
 
 
 class PromoteTask:
     def __init__(self):
         self.name = "Promotion Task"
-        self.errored = False
+        self.did_error = False
         self.error_embed = None
         self.con = sqlite3.connect('./data/discord.db')
 
@@ -23,12 +22,12 @@ class PromoteTask:
         config = Settings.config
         api_key = config.get('hypixel', {}).get('api_key', None)
         if api_key is None:
-            self.errored = True
+            self.did_error = True
             self.error_embed = ConfigErrorEmbed(error_message="No API key detected\nPlease contact my admin!")
             return False
         guild_id = config.get('hypixel', {}).get('guid_id', None)
         if guild_id is None:
-            self.errored = True
+            self.did_error = True
             self.error_embed = ConfigErrorEmbed(error_message="No guild ID detected\nPlease contact my admin!")
             return False
         ids = config.get('discord', {}).get('role_ids', {})
@@ -36,7 +35,7 @@ class PromoteTask:
         api_request = requests.get(uri)
         request_json = api_request.json()
         if not request_json.get('success'):
-            self.errored = True
+            self.did_error = True
             if request_json.get('reason', None) is None:
                 self.error_embed = ApiErrorEmbed(error_message="Unknown Error (code: 5)")
                 return False
@@ -67,7 +66,7 @@ class PromoteTask:
         top_ten_leaderboard = nonzero_leaderboard[:10]
         response_embed = discord.Embed(title="Weekly GEXP Promotions!")
         if len(top_ten_leaderboard) == 0 or len(top_ten_leaderboard) < 10:
-            self.errored = True
+            self.did_error = True
             response_embed.add_field(
                 name="Error",
                 value="An unknown error has occurred. For more information refer to task log: {}")
@@ -80,163 +79,85 @@ class PromoteTask:
         legend = interaction.guild.get_role(ids['guild_legend'])
 
         # Create response message
-        position = 1
+        position = 0
         promotions = []
         for member in top_ten_leaderboard:
+            position += 1
             match position:
                 case 1:
                     first_gexp = member.get('weekly_gexp')
-                    first_playername = member.get('name')
-                    command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
-                    execute = self.con.execute(command)
-                    response = execute.fetchone()
-                    if response is not None:
-                        first_playername = interaction.guild.get_member(response[0]).mention
-                    else:
-                        first_playername = member.get('name').replace("_", "\\_")
-                    position += 1
+                    first_playername = member.get('name').replace("_", "\\_")
                 case 2:
                     second_gexp = member.get('weekly_gexp')
-                    second_playername = member.get('name')
-                    command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
-                    execute = self.con.execute(command)
-                    response = execute.fetchone()
-                    if response is not None:
-                        second_playername = interaction.guild.get_member(response[0]).mention
-                    else:
-                        second_playername = member.get('name').replace("_", "\\_")
-                    position += 1
+                    second_playername = member.get('name').replace("_", "\\_")
                 case 3:
                     third_gexp = member.get('weekly_gexp')
-                    third_playername = member.get('name')
-                    command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
-                    execute = self.con.execute(command)
-                    response = execute.fetchone()
-                    if response is not None:
-                        third_playername = interaction.guild.get_member(response[0]).mention
-                    else:
-                        third_playername = member.get('name').replace("_", "\\_")
-                    position += 1
+                    third_playername = member.get('name').replace("_", "\\_")
                 case 4:
                     fourth_gexp = member.get('weekly_gexp')
-                    fourth_playername = member.get('name')
-                    command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
-                    execute = self.con.execute(command)
-                    response = execute.fetchone()
-                    if response is not None:
-                        fourth_playername = interaction.guild.get_member(response[0]).mention
-                    else:
-                        fourth_playername = member.get('name').replace("_", "\\_")
-                    position += 1
+                    fourth_playername = member.get('name').replace("_", "\\_")
                 case 5:
                     fifth_gexp = member.get('weekly_gexp')
-                    fifth_playername = member.get('name')
-                    command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
-                    execute = self.con.execute(command)
-                    response = execute.fetchone()
-                    if response is not None:
-                        fifth_playername = interaction.guild.get_member(response[0]).mention
-                    else:
-                        fifth_playername = member.get('name').replace("_", "\\_")
-                    position += 1
+                    fifth_playername = member.get('name').replace("_", "\\_")
                 case 6:
                     sixth_gexp = member.get('weekly_gexp')
-                    sixth_playername = member.get('name')
-                    command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
-                    execute = self.con.execute(command)
-                    response = execute.fetchone()
-                    if response is not None:
-                        sixth_playername = interaction.guild.get_member(response[0]).mention
-                    else:
-                        sixth_playername = member.get('name').replace("_", "\\_")
-                    position += 1
+                    sixth_playername = member.get('name').replace("_", "\\_")
                 case 7:
                     seventh_gexp = member.get('weekly_gexp')
-                    seventh_playername = member.get('name')
-                    command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
-                    execute = self.con.execute(command)
-                    response = execute.fetchone()
-                    if response is not None:
-                        seventh_playername = interaction.guild.get_member(response[0]).mention
-                    else:
-                        seventh_playername = member.get('name').replace("_", "\\_")
-                    position += 1
+                    seventh_playername = member.get('name').replace("_", "\\_")
                 case 8:
                     eighth_gexp = member.get('weekly_gexp')
-                    eighth_playername = member.get('name')
-                    command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
-                    execute = self.con.execute(command)
-                    response = execute.fetchone()
-                    if response is not None:
-                        eighth_playername = interaction.guild.get_member(response[0]).mention
-                    else:
-                        eighth_playername = member.get('name').replace("_", "\\_")
-                    position += 1
+                    eighth_playername = member.get('name').replace("_", "\\_")
                 case 9:
                     ninth_gexp = member.get('weekly_gexp')
-                    ninth_playername = member.get('name')
-                    command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
-                    execute = self.con.execute(command)
-                    response = execute.fetchone()
-                    if response is not None:
-                        ninth_playername = interaction.guild.get_member(response[0]).mention
-                    else:
-                        ninth_playername = member.get('name').replace("_", "\\_")
-                    position += 1
+                    ninth_playername = member.get('name').replace("_", "\\_")
                 case 10:
                     tenth_gexp = member.get('weekly_gexp')
-                    tenth_playername = member.get('name')
-                    command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
-                    execute = self.con.execute(command)
-                    response = execute.fetchone()
-                    if response is not None:
-                        tenth_playername = interaction.guild.get_member(response[0]).mention
-                    else:
-                        tenth_playername = member.get('name').replace("_", "\\_")
-                    position += 1
+                    tenth_playername = member.get('name').replace("_", "\\_")
+            #   END OF SWITCH STATEMENT   #
 
-        response_message = f"Here are the weekly promotions!\n\n" \
-                           f"        {champion.mention}\n" \
-                           f"-------------------------------\n" \
-                           f"`#1`   **{first_playername}**: " + '{:,}'.format(first_gexp) + " GEXP\n" \
-                           f"-------------------------------\n" \
-                           f"\n" \
-                           f"       {celestial.mention}\n" \
-                           f"`#2` **{second_playername}**: " + '{:,}'.format(second_gexp) + " GEXP\n" \
-                           f"`#3` **{third_playername}**: " + '{:,}'.format(third_gexp) + " GEXP\n" \
-                           f"\n" \
-                           f"       {legend.mention}\n" \
-                           f"`#4` **{fourth_playername}** : " + '{:,}'.format(fourth_gexp) + " GEXP\n" \
-                           f"`#5` **{fifth_playername}** : " + '{:,}'.format(fifth_gexp) + " GEXP\n" \
-                           f"`#6` **{sixth_playername}** : " + '{:,}'.format(sixth_gexp) + " GEXP\n" \
-                           f"`#7` **{seventh_playername}** : " + '{:,}'.format(seventh_gexp) + " GEXP\n" \
-                           f"`#8` **{eighth_playername}** : " + '{:,}'.format(eighth_gexp) + " GEXP\n" \
-                           f"`#9` **{ninth_playername}** : " + '{:,}'.format(ninth_gexp) + " GEXP\n" \
-                           f"`#10` **{tenth_playername}** : " + '{:,}'.format(tenth_gexp) + " GEXP\n\n\n" \
-                           f"Congratulations to everyone who achieved a rank!"
+            # Queue promotion
+            command = f"SELECT discord_id FROM discord_link WHERE player_uuid IS '{member.get('uuid')}'"
+            execute = self.con.execute(command)
+            response = execute.fetchone()
+            if response is not None:
+                discord_id = response[0]
+                role = config['discord']['role_ids']['guild_legend']
+                if position == 1:
+                    role = config['discord']['role_ids']['guild_champion']
+                elif position == 2 or position == 3:
+                    role = config['discord']['role_ids']['guild_celestial']
 
-        response_value = f"        {champion.mention}\n" \
-                         f"-------------------------------\n" \
-                         f"`#1`   **{first_playername}**: " + '{:,}'.format(first_gexp) + " GEXP\n" \
-                         f"-------------------------------\n" \
-                         f"\n" \
-                         f"       {celestial.mention}\n" \
-                         f"`#2` **{second_playername}**: " + '{:,}'.format(second_gexp) + " GEXP\n" \
-                         f"`#3` **{third_playername}**: " + '{:,}'.format(third_gexp) + " GEXP\n" \
-                         f"\n" \
-                         f"       {legend.mention}\n" \
-                         f"`#4` **{fourth_playername}** : " + '{:,}'.format(fourth_gexp) + " GEXP\n" \
-                         f"`#5` **{fifth_playername}** : " + '{:,}'.format(fifth_gexp) + " GEXP\n" \
-                         f"`#6` **{sixth_playername}** : " + '{:,}'.format(sixth_gexp) + " GEXP\n" \
-                         f"`#7` **{seventh_playername}** : " + '{:,}'.format(seventh_gexp) + " GEXP\n" \
-                         f"`#8` **{eighth_playername}** : " + '{:,}'.format(eighth_gexp) + " GEXP\n" \
-                         f"`#9` **{ninth_playername}** : " + '{:,}'.format(ninth_gexp) + " GEXP\n" \
-                         f"`#10` **{tenth_playername}** : " + '{:,}'.format(tenth_gexp) + " GEXP\n\n\n" \
-                         f"Congratulations to @everyone who achieved a rank!"
+                promotions.append({'member_id': discord_id, 'role_id': role})
+
+        embed_content = f"        {champion.mention}\n" \
+                        f"-------------------------------\n" \
+                        f"`#1`   **{first_playername}**: " + '{:,}'.format(first_gexp) + " GEXP\n" \
+                        f"-------------------------------\n" \
+                        f"\n" \
+                        f"       {celestial.mention}\n" \
+                        f"`#2` **{second_playername}**: " + '{:,}'.format(second_gexp) + " GEXP\n" \
+                        f"`#3` **{third_playername}**: " + '{:,}'.format(third_gexp) + " GEXP\n" \
+                        f"\n" \
+                        f"       {legend.mention}\n" \
+                        f"`#4` **{fourth_playername}** : " + '{:,}'.format(fourth_gexp) + " GEXP\n" \
+                        f"`#5` **{fifth_playername}** : " + '{:,}'.format(fifth_gexp) + " GEXP\n" \
+                        f"`#6` **{sixth_playername}** : " + '{:,}'.format(sixth_gexp) + " GEXP\n" \
+                        f"`#7` **{seventh_playername}** : " + '{:,}'.format(seventh_gexp) + " GEXP\n" \
+                        f"`#8` **{eighth_playername}** : " + '{:,}'.format(eighth_gexp) + " GEXP\n" \
+                        f"`#9` **{ninth_playername}** : " + '{:,}'.format(ninth_gexp) + " GEXP\n" \
+                        f"`#10` **{tenth_playername}** : " + '{:,}'.format(tenth_gexp) + " GEXP\n\n\n" \
+                        f"Congratulations to {interaction.guild.default_role.mention} who achieved a rank!"
+
+        today = datetime.today()
+        last_week = today - timedelta(days=7)
 
         response_embed_files = []
         response_embed = discord.Embed(title="Weekly rank promotions!")
-        response_embed.add_field(name="Ranks for week of {week1}-{week2}", value=response_value)
+        response_embed.add_field(
+            name="Ranks for week of {}-{}".format(today.strftime("%m/%d/%Y"), last_week.strftime("%m/%d/%Y")),
+            value=embed_content
+        )
         response_embed.colour = discord.Colour(0xfa1195)
         response_embed.timestamp = datetime.now(tz=pytz.timezone('EST'))
         response_embed_files.append(discord.File("./data/images/itzilly-icon.png", filename="itzilly-icon.png"))
@@ -257,8 +178,10 @@ class PromoteTask:
                 logging.debug(f"Removed role @legend from {user.display_name}")
 
         # Promotions
-        for todo in promotions:
-            await interaction.guild.get_member(todo.get('user_id')).add_roles(todo.get('role_id'))
+        for task in promotions:
+            _discord_id = task.get('discord_id')
+            _role_id = task.get('role_id')
+            await interaction.guild.get_member(_discord_id).add_roles(_role_id)
         await interaction.response.send_message(embed=response_embed, files=response_embed_files)
 
         logging.debug("Finished task: Promotions")
