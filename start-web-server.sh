335b7c3c30fa18d129e25806cbb8f8104b1ba8b8
illyum <90023277+itzilly@users.noreply.github.com>
2023-02-15 01:25:11 -0700

    Update bash file

--- start-web-server.sh
+++ start-web-server.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-cd web-server/env/bin
-source activate
-cd ../..
+VENV_DIR="web-server/env"
 
+python_version=$(python3 -c "import platform; print(platform.python_version())")
+required_version="3.8.0"
+
+if [ "$(printf '%s\n' "$required_version" "$python_version" | sort -V | head -n1)" != "$required_version" ]; then
+    echo "Python version must be at least $required_version"
+    exit 1
+fi
+
+if [ ! -d "$VENV_DIR" ]; then
+    python3 -m venv "$VENV_DIR"
+fi
+
+source "$VENV_DIR/bin/activate"
+
+# Check for installed packages
 requirements_installed=true
 while read -r package; do
     pip list --format=freeze | grep -i "$package" >/dev/null
@@ -17,6 +30,6 @@ if [ "$requirements_installed" = false ]; then
     pip install -r web-server/requirements.txt
 fi
 
-# Start the server
+# Start web server
 cd web-server/src
 python main.py
