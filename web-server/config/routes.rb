9f115b6d3027f8065db9f143f3d484e8ea702cc2
illyum <itzillyum@gmail.com>
2023-03-24 00:03:43 -0600

    Add leaderboard page

--- web-server/config/routes.rb
+++ web-server/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  # get 'leaderboard/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/articles", to: "articles#index"
+
+  get '/leaderboards(/:type)', to: 'leaderboards#index', as: 'leaderboards', start_date: /\d{4}-\d{2}-\d{2}/, end_date: /\d{4}-\d{2}-\d{2}/
 
   # Defines the root path route ("/")
   # root "articles#index"
