68ea2e516a60cd38fa8d21fef08530f3c86757cf
illyum <itzillyum@gmail.com>
2023-03-31 05:25:39 -0600

    Add news page

---
+++ web-server/app/controllers/pages/news_controller.rb
@@ -0,0 +1,2 @@
+class Pages::NewsController < ApplicationController
+end
--- web-server/config/routes.rb
+++ web-server/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'news', to: 'news#news'
   # get 'leaderboard/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/articles", to: "articles#index"
+  # get "/articles", to: "articles#index"
 
   get '/leaderboards(/:type)', to: 'leaderboards#index', as: 'leaderboards', start_date: /\d{4}-\d{2}-\d{2}/, end_date: /\d{4}-\d{2}-\d{2}/
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'pages#home'
 end
